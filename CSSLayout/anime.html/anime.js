a0c7411e1ce9d5958a0a82248573a0cd8769b199
ducanh97jp <ducanhnguyen.19941997@gmail.com>
2022-09-02 18:13:09 +0900

    anime

---
+++ CSSLayout/anime.html/anime.js
@@ -0,0 +1,59 @@
+let film='';
+function dau_pha() {
+    film = '<iframe width="100%" height="500px" src="https://www.youtube.com/embed/9EmxTHD1W8Y" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>' 
+    + '<b>Đấu phá thương khung -</b>' 
+    + '<small>1.237k lượt xem</small>' 
+    + '<p> Tác giả:<b> Thiên Tàm Thổ Đậu</b></p>' 
+    + '<b>Giới thiệu truyện:</b>' 
+    + '<p>Đấu Phá Thương Khung là một câu chuyện huyền huyễn đặc sắc kể về Tiêu Viêm, một thiên chi kiêu tử với thiên phú tu luyện mà ai ai cũng hâm mộ, bỗng một ngày người mẹ mất đi đễ lại di vật là một chiếc giới chỉ màu đen nhưng từ khi đó Tiêu Viêm đã mất đi thiên phú tu luyện của mình.Từ thiên tài rớt xuống làm phế vật trong 3 năm, rồi bị vị hôn thê thẳng thừng từ hôn, làm dấy lên ý chí nam nhi của mình, Tiêu Viêm nhờ di vật của mẫu thân để lại là 1 chiếc hắc giới chỉ (nhẫn màu đen)Tiêu Viêm gặp được hồn của Dược Lão (Dược Trần – Dược tôn giả) 1 đại luyện dược tông sư của đấu khí đại lục…</p>';
+    document.getElementById("main").innerHTML = film;
+}
+function dau_la() {
+    film = '<iframe width="100%" height="500px" src="https://www.youtube.com/embed/KUx2-qDLOng" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>' 
+    + '<b>Đấu la đại luc -</b>'
+    + '<small>750k lượt xem</small>'
+    + '<p> Tác giả:<b> Đường Gia Tam Thiếu</b></p>'
+    + '<b>Giới thiệu truyện:</b>' 
+    + '<p>Một đại lục không hề yên bình, một cuộc sống đầy hiểm nguy, phiêu lưu nhưng cũng không kém phần lãng mạn. tình yêu, thù hận, trách nhiệm… Tiếp bước những tiền bối đi trước, Hoắc Vũ Hạo và đời sau sử lai khắc thất quái, bằng niềm tin nhiệt huyết tuổi trẻ đã gây dựng lại đường môn tái lập những huy hoàng xưa kia của các tiền bối đi trước…</p>';
+    document.getElementById("main").innerHTML = film;
+}
+function chu_tich() {
+    film = '<iframe width="100%" height="500px" src="https://www.youtube.com/embed/V1Joha427Is" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
+    + '<b>Chủ tịch ngự linh sư -</b>'
+    + '<small>50k lượt xem</small>'
+    + '<p> Tác giả:<b> Tiêu Tương Túy Vũ</b></p>'
+    + '<b>Giới thiệu truyện:</b>' 
+    + '<p>Kiếp trước, nàng là “Quái vật” duy nhất còn sống sót trong cuộc thí nghiệm bí mật, nàng tàn nhẫn, không tiếc liều cả tính mạng bản thân cũng phải báo thù vì mình và đồng bạn bị chết oan!.Tỉnh lại ở dị thế, nàng trở thành người không cha không mẹ, huynh trưởng mất tích, chịu đựng mọi sự khinh bỉ khi dễ của thân thích, làm một đại tiểu thư vô dụng. Tình cảnh càng khó khăn, ý chí chiến đấu của nàng càng dâng trào. Nàng thề đời này nhất định phải trôi qua muôn màu muôn vẻ, đứng ở ngọn núi cao nhất cười kiêu ngạo nhìn cuộc đời. Về phần đám ruồi bọ dám can đảm quấy phá sự thanh tĩnh của nàng, tất cả đều đưa đến địa ngục là tốt nhất...</p>';
+    document.getElementById("main").innerHTML = film;
+}
+function gio_noi() {
+    film = '<iframe width="100%" height="500px" src="https://www.youtube.com/embed/QDZIwm3OeXY" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
+    + '<b>Gió nổi -</b>'
+    + '<small>50k lượt xem</small>'
+    + '<p>Đạo diễn:<b> Hayao Miyazaki</b></p>'
+    + '<b>Giới thiệu truyện:</b>' 
+    + '<p>Đây là một bộ phim anime buồn hay nhất và có lẽ là tác phẩm cuối cùng của đạo diễn Miyazaki. Bộ phim hoạt hình là một thế giới trong sáng và huyền diệu của tuổi trẻ khi kể về cuộc đời của một chàng trai tên Horikoshi đầy nhiệt huyết. Anh đã phát minh ra chiếc máy bay chiến đấu Mitsubishi A6M Zero nổi tiếng trong chiến tranh thế giới thứ II vốn gắn liền với những chàng trai Kamikaze cảm tử. Những lần bay thử và những sức ép từ quân đội cùng căn bệnh lao quái ác đã nhiều lần ngăn cản ước mơ của của chàng trai trẻ… Kết phim anime tuy buồn nhưng rất đáng để người xem suy ngẫm về lý tưởng và tuổi trẻ, những cái giá phải trả khi quyết tâm theo đuổi đam mê của riêng mình. Hẳn bộ phim cũng rất đồng cảm với nhiều tâm trạng khán giả về những năm tháng tuổi trẻ đầy nhiệt huyết và cũng đầy gian nan thử thách...</p>';
+    document.getElementById("main").innerHTML = film;
+}
+function thieu_nien() {
+     film = '<iframe width="100%" height="500px" src="https://www.youtube.com/embed/9jUrkIaFgXU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
+     + '<b>Thiếu niên cẩm y vệ -</b>'
+    + ' <small>344k lượt xem</small>'
+     + '<p> Tác giả:<b> Miêu Khiêu</b></p>'
+     + '<b>Giới thiệu truyện:</b>' 
+     + '<p>Những cuộc phiêu lưu, những tình tiết truyện hấp dẫn, những thứ đan xen giữa thế giới hiện đại, cổ xưa trong truyện Cẩm Y Vệ làm bạn đọc sẽ không khỏi không hứng thú. Đây là một truyện được tác giả xây dựng tổng hợp nhiều yếu tố, nhờ vào trí tưởng tượng, vốn hiểu biết của mình, Miêu Khiêu đã hình thành một truyện lịch sử, quân sự độc đáo. Không còn là một truyện cứng nhắc, khô khan, với những thời gian địa điểm có sẵn nhưng đó là sự linh hoạt, sống động lồng ghép cả tính chất truyện kiếm hiệp vào tác phẩm...</p>'
+
+    + '<p>Truyện xoay quanh Tần Lâm đến từ thế kỷ hai mươi mốt, là một cao thủ trinh sát hình sự nhiều lần phá đại án, kỹ thuật pháp y tinh xảo, bốn năm học đại học Công An Nhân Dân Hoa Hạ hệ chính quy khoa điều tra kỹ thuật hình sự. Sau công tác tại học viện cảnh sát hình sự Hoa Hạ tiếp tục học lấy được bằng Thạc sĩ pháp y, trong lúc nhậm chức nhiều lần phá được án lớn, quan trọng do bộ Công An đốc thúc, chỉ mấy năm đã thăng tiến thành Cảnh Đốc cấp hai...</p>'
+     
+    + '<p>Không ngờ trong một lần thi hành nhiệm vụ phát sinh tai nạn xe cộ, cả người lẫn xe rơi xuống vực sâu trăm trượng. Đến khi tỉnh lại phát hiện ra toàn thân mình lõa thể nằm ngoài vùng hoang dã, thân thể đã hóa thành bộ dáng thiếu niên chỉ mới mười lăm mười sáu tuổi. Sau mới phát hiện thì ra mình đã trở lại triều Minh, Tần Lâm sẽ làm thế nào để tồn tại giữa một thế giới vô cùng xa lạ; có thể hay không tồn tại và đứng vững, có hay không sẽ bị lung lạc...</p>';
+    document.getElementById("main").innerHTML = film;
+}
+function vung_dat() {
+    film = '<iframe width="100%" height="500px" src="https://www.youtube.com/embed/ByXuk9QqQkk" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
+    + '<b>Vùng đất linh hồn -</b>'
+    + '<small>274k lượt xem</small>'
+    + '<p>Đạo diễn:<b> Hayao Miyazaki</b></p>'
+    + '<b>Giới thiệu truyện:</b>' 
+    + '<p>Nhắc đến đầu tiên trong top những bộ phim Anime hay nhất mọi thời đại, Spirited Away là một cái tên sáng giá khi đạt giải Oscar và 51 các giải thưởng khác cùng 22 đề cử. Đây là câu chuyện kể về cuộc gặp gỡ của Chihiro (cô bé 10 tuổi) và cậu bé bí ẩn tên là Haku, người hứa sẽ giúp đỡ cô. Vì không may, trong quá trình chuyển đến sống ở vùng ngoại ô cô và gia đình đã vô tình tìm ra một vùng đất ma thuật. Bố mẹ cô bị biến thành heo để cô phải lang thang một mình trong thế giới ma thuật được cai trị bởi các vị thần, các phù thủy và linh hồn. Khi gặp được Haku, cô được anh giúp đỡ tận tình và cho cô một công việc trong một nhà tắm của hàng ngàn vị thần và linh hồn Nhật Bản. Hành trình của Chihiro là vượt qua những trở ngại để có thể phá giải lời nguyền cho bố mẹ, đưa họ trở về trước khi ba mẹ cô trở thành bữa tối cho các vị khách.</p>';
+    document.getElementById("main").innerHTML = film;
+}
